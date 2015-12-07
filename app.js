d9e6ca271f370021abf42f66a282f3ef8b0af8aa
panox <panospalos@gmail.com>
2015-12-07 10:49:43 +0000

    shows stuff

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+app.set('views');
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res) {
+
+  res.render('home')
+
+});
+
+app.listen(process.env.PORT || 3000 )
