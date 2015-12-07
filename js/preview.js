b88288f27abb85832f4e040cc2b6feb8dbb0baf8
panox <panospalos@gmail.com>
2015-12-07 16:31:11 +0000

    changes effects

--- js/preview.js
+++ js/preview.js
@@ -2,7 +2,7 @@ $(function() {
   $("#selected-effect").change(function() {
     console.log('click')
     var effect = $('#selected-effect').val()
-    $("#preview").attr("src","image-" + effect + ".jpeg");
+    $("#preview").attr("src","/css/images/effects/image-" + effect + ".jpeg");
   })
 });
 
