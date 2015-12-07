222f243ab5c8103a1e71c532d9f694ba74fe3906
benalun-jones <benalunjones@gmail.com>
2015-12-07 14:27:28 +0000

    added hompage layout basic

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views');
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
 
 
 app.get('/', function(req, res) {
