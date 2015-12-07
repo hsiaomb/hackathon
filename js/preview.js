72449307de54f650b398cdc04d112ddd46a849ea
panox <panospalos@gmail.com>
2015-12-07 15:46:56 +0000

    jquery load function

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,8 @@
-$('#recipe-serves').change(function() {
-  var effect = 
-  $('#preview').html( '<img src="http://localhost:3000/200/200/'+ +'">');
+$(function() {
+  $("#recipe-serves").change(function() {
+    console.log('click')
+    var effect = $('#recipe-serves').val()
+    $('#preview').html( '<img src="http://localhost:3000/200/200/'+ effect +'">');
+  })
 });
+
